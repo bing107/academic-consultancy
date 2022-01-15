f53e8ae2aad8db957f6bbb771b936986594f5b3e
hassan syed <hassan@hassans-air.lan>
2022-01-15 22:24:40 +0100

    chore: add ids to env file

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,20 @@
+require("dotenv").config({
+  path: `.env`,
+})
+
 module.exports = {
     siteMetadata: {
       title: `EU Academic Consultancy`,
         siteUrl: `https://www.yourdomain.tld`,
     },
     plugins: [
-
+      {
+        resolve: `gatsby-source-contentful`,
+        options: {
+          spaceId: process.env.SPACE_ID,
+          accessToken: process.env.ACCESS_TOKEN,
+        },
+      },
+      `gatsby-plugin-image`
     ]
 }
